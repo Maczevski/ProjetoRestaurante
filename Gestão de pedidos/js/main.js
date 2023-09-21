a60af5fb899669b17b227c1386fc355933a40c57
Maczevski <maczevski@gmail.com>
2023-09-21 18:24:20 -0300

    Almost done

--- Gestão de pedidos/js/main.js
+++
@@ -1,7 +0,0 @@
-function enviarPedido() {
-    const mesa = document.getElementById('mesa').value;
-    const observacoes = document.getElementById('observacoes').value;
-
-    // Feche o modal após o pedido ser enviado ou processado
-    $('#pedidoModal').modal('hide');
-}
