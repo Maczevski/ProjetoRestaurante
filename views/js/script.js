00be80874a7e6778bc049854fea43eaf8d23b69f
Maczevski <maczevski@gmail.com>
2023-10-06 18:32:16 -0300

    Alteração no controle de estoque

--- views/js/script.js
+++ views/js/script.js
@@ -2,6 +2,7 @@ var modal = document.getElementById('myModal');
 var abrirModal = document.getElementById('adicionar-produto');
 var fecharModal = document.getElementsByClassName('fechar')[0];
 
+
 // Função para abrir o modal
 abrirModal.onclick = function() {
     modal.style.display = 'block';
@@ -29,3 +30,34 @@ window.onclick = function(event) {
         modal.style.display = 'none';
     }
 }
+
+
+function adicionarProduto() {
+    var nomeProduto = document.getElementById('campoNome').value;
+    var quantidadeProduto = document.getElementById('campoQntd').value;
+
+    if (nomeProduto && quantidadeProduto >= 0) {
+        var tabela = document.getElementById('tabela_estoque');
+        var newRow = tabela.insertRow(tabela.rows.length);
+
+        var cell1 = newRow.insertCell(0);
+        cell1.classList.add("nome-produto");
+        var cell2 = newRow.insertCell(1);
+        cell2.classList.add("quantidade");
+        var cell3 = newRow.insertCell(2);
+        cell3.classList.add("excluir-produto");
+
+        cell1.innerHTML = nomeProduto;
+        cell2.innerHTML = '<input class="table-content centralizado" type="number" value="' + quantidadeProduto + '" min="0">';
+        cell3.innerHTML = '<button onclick="excluirProduto(this)"><img src="img/x.png" alt="x" class="x"></button>';
+        
+        document.getElementById('campoNome').value = '';
+        document.getElementById('campoQntd').value = '';
+    }
+}
+
+function excluirProduto(button) {
+    var row = button.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
+
